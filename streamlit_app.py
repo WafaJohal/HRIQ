4c7a48d7bac154f7dc5bf70fb23960f77054708f
Wafa Johal <wafa.johal@gmail.com>
2022-03-17 15:44:18 +1100

    adding wordcloud

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,16 @@ import streamlit as st
 import pandas as pd
 from st_aggrid import AgGrid, GridOptionsBuilder
 from st_aggrid.shared import GridUpdateMode
-#import streamlit_wordcloud as wordcloud
-
-
+import streamlit_wordcloud as st_wordcloud
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+from nltk.corpus import stopwords
+import collections
 
 header_container = st.container()
+wordcloud_container = st.container()
 dataset_container =  st.container()
 
+survey_df = pd.read_excel("hri_metrics.xlsx", sheet_name="Named Surveys")
 
 def aggrid_interactive_table(df: pd.DataFrame):
     """Creates an st-aggrid interactive table based on a dataframe.
@@ -40,12 +43,33 @@ def aggrid_interactive_table(df: pd.DataFrame):
 
 with header_container:
     st.title("HRI Questionnaires")
-    st.text('this webpage is meant to navigate an search for previously used questionnaire in HRI')
+    st.text("""This webpage is meant to navigate and search for previously used questionnaire in HRI.
+    It is based in data from https://zenodo.org/record/5789410#.Ydc98WjMKUk%20%5B1%5D.
+    """)
+
+with wordcloud_container:
+    st.header("Wordcloud of keywords used in the dataset")
+    text = " ".join(name for name in survey_df.Survey)
+    # stop words list
+    stop = stopwords.words('english')
+    stop.extend(["inventory", "scale", "index", "questionnaire", "evaluation"," assessment","measure","questionaire","robot","measurment"])
+
+    # Create and generate a word cloud image:
+    survey_df.cleanSurvey = survey_df.Survey.str.lower().str.strip().str.split()
+    survey_df['Clean'] = survey_df.cleanSurvey.apply(lambda x: [w.strip() for w in x if w.strip() not in stop])
+    survey_df['Clean'] = pd.DataFrame( survey_df['Clean'])
+
+    words = survey_df.Clean.tolist()
+    flat_list = [item for sublist in words for item in sublist]
+    wdic = [dict(text = i, value = flat_list.count(i)) for i in set(flat_list)]
+
+    wc = st_wordcloud.visualize(words=wdic)
+    st.write(wc)
 
 
 with dataset_container:
     st.header('Loading the dataset')
-    survey_df = pd.read_excel("hri_metrics.xlsx", sheet_name="Named Surveys")
+  
     selection = aggrid_interactive_table(df=survey_df)
 
     if selection:
